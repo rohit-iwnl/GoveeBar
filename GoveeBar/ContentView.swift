2862fcd70f5b6354108a6a212e2307b3854291ca
rohit-iwnl <rohitmanivel9@gmail.com>
2025-10-27 20:46:25 -0700

    initial commit

---
+++ GoveeBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoveeBar
+//
+//  Created by Rohit Manivel on 10/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoveeBar/GoveeBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoveeBarApp.swift
+//  GoveeBar
+//
+//  Created by Rohit Manivel on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GoveeBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
