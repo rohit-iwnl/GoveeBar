60b234263e525f6b710efadc1af77ceb288b8202
rohit-iwnl <rohitmanivel9@gmail.com>
2025-10-27 20:59:47 -0700

    refactor folder structure and setup menubar

--- GoveeBar/Core/Views/ContentView.swift
+++ GoveeBar/Core/Views/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("GoveeBar")
+                .font(.title)
+                .fontWeight(.bold)
+            
+        
         }
         .padding()
     }
