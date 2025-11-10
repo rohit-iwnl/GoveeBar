f37d301f5219531bf9640bda2fa4710070970eb9
rohit-iwnl <rohitmanivel9@gmail.com>
2025-11-09 19:17:10 -0700

    initial commit

---
+++ GoveeBar/GoveeBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoveeBarApp.swift
+//  GoveeBar
+//
+//  Created by Rohit Manivel on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GoveeBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
